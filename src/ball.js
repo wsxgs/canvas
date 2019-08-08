96f3dd5207aee9a45875a46c646eb37374b21663
wangzhiqiang <1071683940@qq.com>
2019-08-08 14:39:44 +0800

    perf：canvas优化
    # head: <type>: <subject>
    # - type:
    # - feat：添加新功能。
    # - fix：修复 bug。
    # - style：代码格式。
    # - perf：优化相关。
    # - revert：分支回溯。
    # - chore：构建配置相关。
    # - docs：文档相关。
    # - build: 打包相关
    
    # - subject: start with verb (such as 'change'), 50-character line

--- src/ball.js
+++ src/ball.js
@@ -1,89 +1,93 @@
 /**
  * canvas绘制随机小球
- * @param {*} ballNum
+ * @param {Number} number 小球数量
+ * @param {Number} size 小球最大半径
+ * @param {Number} velocity 小球移动速度
  */
-function drawBall (ballNum = 100) {
+function drawBall (config = {
+  number: 100,
+  size: 50,
+  velocity: 2
+}) {
   const canvas = document.getElementById('canvas')
   const screenW = document.clientWidth || document.body.clientWidth
   const screenH = document.clientHeight || document.body.clientHeight
-  // const dpr = window.devicePixelRatio;
-  // const canvasW = dpr * screenW;
-  // const canvasH = dpr * screenH;
+  const dpr = window.devicePixelRatio
+  const canvasW = dpr * screenW
+  const canvasH = dpr * screenH
 
   const ctx = canvas.getContext('2d')
-  const ballList = []
+  let ballList = []
 
-  canvas.width = screenW
-  canvas.height = screenH
+  canvas.width = canvasW
+  canvas.height = canvasH
 
-  // 小球实例
-  class Ball {
-    constructor () {
-      this.x = Math.random() * screenW
-      this.y = Math.random() * screenH
-      this.radius = Math.random() * 50
-      this.vx = Math.random() > 0.5 ? Math.random() : -Math.random()
-      this.vy = Math.random() > 0.5 ? Math.random() : -Math.random()
-      this.diapha = Math.random().toFixed(1)
-      this.style = `rgba(
-        ${Math.floor(Math.random() * 255)},
-        ${Math.floor(Math.random() * 255)},
-        ${Math.floor(Math.random() * 255)},
-        ${this.diapha}
-      )`
-    }
-
-    // 绘制小球
-    draw () {
-      ctx.beginPath()
-      ctx.fillStyle = this.style
-      const nextX = this.x + this.vx
-      const nextY = this.y + this.vy
-      if (nextX < 0 || nextX > screenW) {
-        this.vx = -this.vx
-        this.n = 0
-      }
-      if (nextY < 0 || nextY > screenH) {
-        this.vy = -this.vy
-        this.n = 0
-      }
-      this.x += this.vx
-      this.y += this.vy
-
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
-      ctx.fill()
-    }
-  }
+  initBalls()
 
   // 创建小球
-  for (let i = 0; i < ballNum; i++) {
-    ballList[i] = new Ball()
-    ballList[i].draw()
+  function initBalls () {
+    ballList = []
+    for (let i = 0; i < config.number; i++) {
+      ballList[i] = {
+        x: Math.random() * canvasW,
+        y: Math.random() * canvasH,
+        radius: Math.random() * config.size,
+        vx: Math.random() > 0.5 ? Math.random() * config.velocity : -Math.random() * config.velocity,
+        vy: Math.random() > 0.5 ? Math.random() * config.velocity : -Math.random() * config.velocity,
+        diapha: Math.random().toFixed(1)
+      }
+      ballList[i].style = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${ballList[i].diapha})`
+      draw(ballList[i])
+    }
+    ballMove()
+  }
+
+  // 绘制小球
+  function draw (ball) {
+    ctx.beginPath()
+    ctx.fillStyle = ball.style
+    const nextX = ball.x + ball.vx
+    const nextY = ball.y + ball.vy
+
+    // 边缘回弹
+    if (nextX < 0 || nextX > canvasW) {
+      ball.vx = -ball.vx
+      ball.n = 0
+    }
+    if (nextY < 0 || nextY > canvasH) {
+      ball.vy = -ball.vy
+      ball.n = 0
+    }
+
+    // 改变小球位置
+    ball.x += ball.vx
+    ball.y += ball.vy
+
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, true)
+    ctx.fill()
   }
 
   // 小球移动
   function ballMove () {
-    ctx.clearRect(0, 0, screenW, screenH)
+    ctx.clearRect(0, 0, canvasW, canvasH)
     ctx.save()
     for (const i in ballList) {
-      ballList[i].draw()
+      draw(ballList[i])
     }
     ctx.restore()
     window.requestAnimationFrame(ballMove)
   }
 
-  window.requestAnimationFrame(ballMove)
-
   // 鼠标经过加速
   canvas.addEventListener('mousemove', e => {
-    const mouseX = e.x
-    const mouseY = e.y
+    const mouseX = e.x * dpr
+    const mouseY = e.y * dpr
     ballList.forEach((item, index) => {
       if (mouseX < item.x + item.radius && mouseX > item.x - item.radius) {
         if (mouseY < item.y + item.radius && mouseY > item.y - item.radius) {
-          if (item.vx > 1) {
-            return
-          }
+          // if (item.vx > 1) {
+          //   return
+          // }
           item.vx += (item.vx / Math.abs(item.vx)) * 5
           item.vy += (item.vy / Math.abs(item.vy)) * 5
           setTimeout(() => {
@@ -94,6 +98,10 @@ function drawBall (ballNum = 100) {
       }
     })
   })
+
+  window.onresize = () => {
+    initBalls()
+  }
 }
 
 export default drawBall
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import drawBall from './ball'
-drawBall()
+import drawBallCanvas from './ball'
+import mouseoverCanvas from  './mouseover'
+
+// 小球移动
+drawBallCanvas({
+	number: 100,
+	size: 50,
+	velocity: 2
+})
+
+// 鼠标跟随
+// mouseoverCanvas()
